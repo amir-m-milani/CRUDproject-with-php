2f76e7303c4804635fd43015366840ce88507dcb
amir-m-milani <amirmilani08@gmail.com>
2023-04-13 17:35:56 +0430

    move form code to _form.php and use it update.php

--- update.php
+++ update.php
@@ -21,46 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     }
 
 
-    // header("Location: index.php");
+    header("Location: index.php");
 }
-?>
 
-<div class="container">
-    <div class="card">
-        <div class="card-header">
-            <h3>Update user :<b><?php echo $user['name'] ?></b></h3>
-        </div>
-        <div class="card-body">
-
-            <form method="POST" enctype="multipart/form-data" action="">
-                <div class="form-group">
-                    <label>Name</label>
-                    <input name="name" value="<?php echo $user['name'] ?>" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Username</label>
-                    <input name="username" value="<?php echo $user['username'] ?>" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label>Email</label>
-                    <input name="email" value="<?php echo $user['email'] ?>" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Phone</label>
-                    <input name="phone" value="<?php echo $user['phone'] ?>" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Website</label>
-                    <input name="website" value="<?php echo $user['website'] ?>" class="form-control">
-                </div>
-                <div class="form-group mt-3">
-                    <label>Image</label>
-                    <input name="picture" type="file" class="form-control-file">
-                </div>
-                <button class="btn btn-success mt-3">Submit</button>
-            </form>
-        </div>
-    </div>
-</div>
-</div>
+include "_form.php";
